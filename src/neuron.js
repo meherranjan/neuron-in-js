9f1017600e1d8e803ebfa8072f8598a433f292e7
Meher Ranjan Howji <hmeherranjan@gmail.com>
2023-03-04 21:06:43 +0530

    wip victory

---
+++ src/neuron.js
@@ -0,0 +1,19 @@
+// input -> process -> output
+// x -> f(x) -> y
+
+function perceptron(x1, x2) {
+  let weightedInputOne = x1 * w1;
+  let weightedInputTwo = x2 * w2;
+}
+
+class Perceptron {
+  constructor(n) {
+    let weights = [];
+    for (let i = 0; i < weights.length; i++) {
+      weights[i] = Math.random();
+    }
+  }
+}
+
+
+predictor([10, 22.04]);
